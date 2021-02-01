d60dff9b9845d680b51532bb41cffdf646ca0382
Andrew Mitchell <andymitch559@gmail.com>
2021-01-31 20:19:53 -0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+var fs = require("fs")
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  if(req.method == 'GET'){
+    fs.readFile('../../ngrok_url.txt', function (err, data) {
+      if (err) {
+        return console.error(err);
+      }
+      res.end(data.toString());
+    });
+  }
+  else {
+    fs.writeFile('../../ngrok_url.txt', req.body, function(err) {
+    if (err) {
+        return console.error(err);
+    }
+  }
 }
